3fcb7505462530406aa0a24d1f4b3864e24dc884
Daniel G <replayattack@pm.me>
2020-07-24 22:58:49 -0500

    Complete toggling the navbar links on mobile lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
+import Navbar from './Navbar'
 
 function App() {
   return (
-    <header className="flex items-center justify-between px-4 py-3 bg-gray-900">
-      <div>
-        <img className="h-8" src="/img/logo-inverted.svg" alt="Workcation" />
+    <div className="antialiased text-gray-900">
+      <div className="bg-gray-200 min-h-screen">
+        <Navbar />
       </div>
-      <div>
-        <button
-          type="button"
-          className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
-        >
-          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-            <path
-              fill-rule="evenodd"
-              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-            />
-          </svg>
-        </button>
-      </div>
-    </header>
+    </div>
   );
 }
 
---
+++ src/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="bg-gray-900">
+      <div className="flex items-center justify-between px-4 py-3">
+        <div>
+          <img className="h-8" src="/img/logo-inverted.svg" alt="Workcation" />
+        </div>
+        <div>
+          <button
+            onClick={(_) => setIsOpen((state) => !state)}
+            type="button"
+            className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+          >
+            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+              {isOpen ? (
+                <path
+                  fillRule="evenodd"
+                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className={`px-2 pt-2 pb-4 ${isOpen ? 'block' : 'hidden'}`}>
+        <a
+          href="#"
+          className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
+        >
+          List your property
+        </a>
+        <a
+          href="#"
+          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
+        >
+          Trips
+        </a>
+        <a
+          href="#"
+          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
+        >
+          Messages
+        </a>
+      </div>
+    </header>
+  );
+}
