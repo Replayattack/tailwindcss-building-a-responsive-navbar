78db5658cc3d7730b5b0411eccb2ee8289ee87cf
Daniel G <replayattack@pm.me>
2020-07-24 22:33:14 -0500

    Complete building a navbar layout with flexbox lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <header className="flex items-center justify-between px-4 py-3 bg-gray-900">
+      <div>
+        <img className="h-8" src="/img/logo-inverted.svg" alt="Workcation" />
+      </div>
+      <div>
+        <button
+          type="button"
+          className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path
+              fill-rule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            />
+          </svg>
+        </button>
+      </div>
+    </header>
   );
 }
 
